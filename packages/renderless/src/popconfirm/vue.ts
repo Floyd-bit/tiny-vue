b6e92211fd440edda9211bad9f993537b4e06a15
floyd-bit <1520246115@qq.com>
2024-01-18 23:49:57 +0800

    style(popconfirm): [popconfirm] improve typescript declaration of components

--- packages/renderless/src/popconfirm/vue.ts
+++ packages/renderless/src/popconfirm/vue.ts
@@ -1,9 +1,19 @@
+import type {
+  IPopconfirmApi,
+  IPopconfirmProps,
+  IPopconfirmRenderlessParamUtils,
+  ISharedRenderlessParamHooks
+} from '@/types'
 import { show, hide, confirm, handleEmit } from './index'
 
 export const api = ['state', 'show', 'hide', 'confirm', 'handleEmit']
 
-export const renderless = (props, { computed, reactive }, { emit, constants, designConfig, vm }) => {
-  const api = {}
+export const renderless = (
+  props: IPopconfirmProps,
+  { computed, reactive }: ISharedRenderlessParamHooks,
+  { emit, constants, designConfig, vm }: IPopconfirmRenderlessParamUtils
+): IPopconfirmApi => {
+  const api = {} as IPopconfirmApi
   const designIcon = designConfig?.icons?.[props.type]
   const state = reactive({
     isLock: false,
